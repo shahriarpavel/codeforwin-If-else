e9fd79150784a7ef945f464973fc7f6da25359f8
shahriarpavel <82736754+shahriarpavel@users.noreply.github.com>
2023-09-29 20:31:08 +0600

    Update Prob-01.c

--- Prob-01.C
+++ Prob-01.C
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main() {
+    int num1,num2,max;
+    printf("Input num1: ");
+    scanf("%d",&num1);
+    printf("Input num2: ");
+    scanf("%d",&num2);
+    if(num1>num2){
+        printf("Number 1 is Maximum");
+    }
+    else{
+        printf("Number 2 is Maximum");
+    }
+    return 0;
+}
