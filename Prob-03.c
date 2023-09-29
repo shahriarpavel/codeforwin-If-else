b25f3750619cef9d39340463b4322c24acefee98
shahriarpavel <82736754+shahriarpavel@users.noreply.github.com>
2023-09-29 20:41:32 +0600

    Create Prob-03.c

---
+++ Prob-03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int num,n;
+    printf("Input number: ");
+    scanf("%d",&num);
+    if(num>0){
+        printf("Number is Positive");
+    }
+    else if (num<0){
+         printf("Number is Negative");
+    }
+    else{
+       printf("Number is Zero");
+    }
+    return 0;
+}
